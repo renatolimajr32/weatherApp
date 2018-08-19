2e8c1bc4d5e8a406471fa7b2cb52500aed7576dc
renatolimajr32 <renatolimajr32@gmail.com>
2018-08-19 20:48:25 -0300

    starting the app

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/jsx-uses-react": [1],
+        "react/jsx-uses-vars": [1],
+        "react/react-in-jsx-scope": [1]
+    }
+};
